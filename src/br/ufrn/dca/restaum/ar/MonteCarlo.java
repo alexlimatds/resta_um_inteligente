3fced28bf7b82367d2211316c5143e5993d2b52f
alexlima <alexlima@2fa15abd-a8b1-6e46-84f7-e18e33f7f7b2>
2008-11-09 15:19:18 +0000

    -ar.MonteCarlo: alteração do algoritmo. Agora todo o episódio é simulado para depois realizar os cálculos necessários (média, Q*, ...). Desta forma foi necessário excluir o objeto ocorrido, criar o objeto paresOcorridos, criar a variável inicioEpisodio e mudar algumas rotinas de lugar.
    git-svn-id: svn://localhost/mestrado/RestaUm@23 2fa15abd-a8b1-6e46-84f7-e18e33f7f7b2

--- src/br/ufrn/dca/restaum/ar/MonteCarlo.java
+++ src/br/ufrn/dca/restaum/ar/MonteCarlo.java
@@ -31,62 +31,80 @@ public class MonteCarlo {
         tabelaQ = new LinkedHashMap<String, TabelaValorEstadoAcao>(); //valores dos pares estado-ação. a chave é o estado
         Map<String, List<Double>> retornosEstadoAcao = new HashMap<String, List<Double>>(); //listas de retornos dos pares estado-ação. a chave é a junção das string de estado e ação separadas por ponto e vírgula
         
-        //utilizado para indicar se um par estado-ação já ocorreu no episódio. a chave é a junção das string de estado e ação separadas por ponto e vírgula
-        Map<String, Boolean> ocorrido = new HashMap<String, Boolean>();
+        Map<String, String> paresOcorridos = new HashMap<String, String>(); //guarda pares estado-ação ocorridos no episódio
         for(int i = 0; i < qtdEpisodios; i++){
             int maxCasasVazias = getMaxCasasVazias();
-            int qtdCasasVazias = (int) Math.round(Math.random() * maxCasasVazias);//gera um número entre 0 e maxCasasVazias
+            int qtdCasasVazias = (int) Math.round(1 + Math.random() * maxCasasVazias);//gera um número entre 1 e maxCasasVazias
             tabuleiro.setQuantidadeInicialDeCasasVazias(qtdCasasVazias);
             tabuleiro.iniciarJogo();
             List<String> acoes = tabuleiro.getMovimentosValidos2();
-            ocorrido.clear(); //limpa as aparições dos pares estado-ação para o novo episódio
+            paresOcorridos.clear();
+            //executa jogadas de acordo com a política enquanto for possível mover peças
+            boolean inicioEpisodio = true;
             while(acoes.size() > 0){
                 String estado = tabuleiro.getRepresentacaoBinaria();
-                String acao = null;
-                if(politica.containsKey(estado)){ //escolhe a ação de acordo com a política
-                    acao = politica.get(estado);
+                //verifica se já há uma tabela de valores para o estado atual
+                TabelaValorEstadoAcao tabela = tabelaQ.get(estado);
+                if(tabela == null){//não há tabela, então a cria
+                    tabela = new TabelaValorEstadoAcao(estado);
+                    for(String acao : acoes){
+                        tabela.inserirValor(acao, 0.0);
+                    }
+                    tabelaQ.put(estado, tabela);
                 }
-                else{ //não há ação definida para o estado de acordo com a política, então usa qualquer ação
-                    acao = acoes.get(0);
+                
+                String acao = null;
+                if(inicioEpisodio){
+                    //escolhe uma ação aleatoriamente
+                    int j = (int)Math.round(0 + Math.random() * (acoes.size() - 1));//gera um número entre 1 e maxCasasVazias
+                    acao = acoes.get(j);
+                    inicioEpisodio = false;
+                }
+                else{
+                    if(politica.containsKey(estado)){ //escolhe a ação de acordo com a política
+                        acao = politica.get(estado);
+                    }
+                    else{ //não há ação definida para o estado de acordo com a política, então usa qualquer ação
+                        int j = (int)Math.round(0 + Math.random() * (acoes.size() - 1));//gera um número entre 1 e maxCasasVazias
+                        acao = acoes.get(j);
+                    }
                 }
                 boolean ok = tabuleiro.moverPeca(acao);
                 if(ok){//ação realizada
-                    //verifica se é a primeira ocorrência deste par estado-ação no episódio
-                    String key = estado + ";" + acao;
-                    if(!ocorrido.containsKey(key)){
-                        ocorrido.put(key, ok);
-                        double valorRetorno = getRetorno(acao);
-                        List<Double> listaRetornos = retornosEstadoAcao.get(key);
-                        if(listaRetornos != null){//verifica se já há uma lista de retornos para o par estado-ação
-                            retornosEstadoAcao.get(key).add(valorRetorno);
-                        }
-                        else{//não há lista de retornos para o par estado-ação, então a cria e insere o valor de retorno
-                            listaRetornos = new ArrayList<Double>();
-                            listaRetornos.add(valorRetorno);
-                            retornosEstadoAcao.put(key, listaRetornos);
-                        }
-                        //calcula a média dos retornos do par estado-ação
-                        double media = 0;
-                        for(Double d : listaRetornos){
-                            media += d;
-                        }
-                        media = media / listaRetornos.size();
-                        //verifica se já há uma tabela de valores para o par estado-ação
-                        TabelaValorEstadoAcao tabela = tabelaQ.get(estado);
-                        if(tabela == null){
-                            tabela = new TabelaValorEstadoAcao(estado);
-                            tabelaQ.put(estado, tabela);
-                        }
-                        tabela.inserirValor(acao, media);
-                        //atualiza a política com a ação com maior valor Q
-                        politica.put(estado, tabela.getAcaoValorMaximo());
-                    }
+                    paresOcorridos.put(estado, acao);
                 }
                 else{//ação não realizada, o que significa que há algum erro de implementação
                     System.out.println("Ação não realizada -> existe algum BUG!!");
                 }
                 
                 acoes = tabuleiro.getMovimentosValidos2();
+            }
+            
+            //percorre os pares estado-ação ocorridos no episódio
+            for(String estado : paresOcorridos.keySet()){
+                String acao = paresOcorridos.get(estado);
+                double valorRetorno = getRetorno(acao);
+                String key = estado + ";" + acao;
+                List<Double> listaRetornos = retornosEstadoAcao.get(key);
+                if(listaRetornos != null){//verifica se já há uma lista de retornos para o par estado-ação
+                    retornosEstadoAcao.get(key).add(valorRetorno);
+                }
+                else{//não há lista de retornos para o par estado-ação, então a cria e insere o valor de retorno
+                    listaRetornos = new ArrayList<Double>();
+                    listaRetornos.add(valorRetorno);
+                    retornosEstadoAcao.put(key, listaRetornos);
+                }
+                //calcula a média dos retornos do par estado-ação
+                double media = 0;
+                for(Double d : listaRetornos){
+                    media += d;
+                }
+                media = media / listaRetornos.size();
+                //atualiza o valor do par estado-ação
+                TabelaValorEstadoAcao tabela = tabelaQ.get(estado);
+                tabela.inserirValor(acao, media);
+                //atualiza a política com a ação com maior valor Q
+                politica.put(estado, tabela.getAcaoValorMaximo());
             }
         }
         
@@ -128,7 +146,7 @@ public class MonteCarlo {
     public static void main(String[] args) {
         FileWriter fWriter = null;
         try {
-            int qtdEpisodios = 300000;
+            int qtdEpisodios = 500000;
             int qtdNiveis = 5;
             Tabuleiro t = new Tabuleiro(qtdNiveis);
             MonteCarlo mc = new MonteCarlo(t);
@@ -148,8 +166,9 @@ public class MonteCarlo {
             fWriter = new FileWriter("tabela-estado-acao.txt");
             for (String key : table.keySet()) {
                 TabelaValorEstadoAcao ta = table.get(key);
-                String acao = ta.getAcaoValorMaximo();
-                fWriter.write(key + " " + ta.getValor(acao) + "\n");
+                for(String acao : ta.getAcoes()){
+                    fWriter.write(key + " " + acao + " " + ta.getValor(acao) + "\n");
+                }
             }
             fWriter.write("\n");
             fWriter.close();
